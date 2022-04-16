c9e1ef5af45ee6054bcdab8b37c8e94b5030b85d
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2022-04-16 14:49:43 +0300

    Initial Commit

---
+++ PhotoSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSocial
+//
+//  Created by Furkan Gençoğulları on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
