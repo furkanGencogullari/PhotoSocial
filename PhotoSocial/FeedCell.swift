3f38242eeb222c8e6a3370d4b8787060ef0b17d0
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2022-04-18 17:17:41 +0300

    Did prototype cell and fixed tab bar color problem

---
+++ PhotoSocial/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  PhotoSocial
+//
+//  Created by Furkan Gençoğulları on 18.04.2022.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
