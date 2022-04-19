d2e3d5847c5b7b4fee93f4cf836d5840aa81d25f
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2022-04-19 10:45:05 +0300

    solved feed cell issue

--- PhotoSocial/TabViewPages/FeedViewController.swift
+++ PhotoSocial/TabViewPages/FeedViewController.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userImageArray = [String]()
+    var userDescriptionArray = [String]()
+    var userLikesArray = [Int]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +29,56 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tabBarController?.tabBar.standardAppearance = appearance
         tabBarController?.tabBar.scrollEdgeAppearance = tabBarController?.tabBar.standardAppearance
         
+        getDataFromFirestore()
+        
 
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.userDescription.text = userDescriptionArray[indexPath.row]
+        cell.likeCountLabel.text = String(userLikesArray[indexPath.row])
+        //cell.imageView?.sd_setImage(with: URL(string: userImageArray[indexPath.row]))
+        cell.imageView?.image = UIImage(named: "image-2")
+        cell.imageView?.contentMode = .scaleAspectFit
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
+    }
+    
+    func getDataFromFirestore() {
+        let db = Firestore.firestore()
+        db.collection("Posts").addSnapshotListener { snapshot, error in
+            if error != nil {
+                
+            } else {
+                if snapshot?.isEmpty != true {
+                    for doc in snapshot!.documents {
+                        let documentID = doc.documentID
+                        print(documentID)
+                        
+                        if let postedBy = doc.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let description = doc.get("description") as? String {
+                            self.userDescriptionArray.append(description)
+                        }
+                        if let likes = doc.get("likes") as? Int {
+                            self.userLikesArray.append(likes)
+                        }
+                        if let image = doc.get("imageUrl") as? String {
+                            self.userImageArray.append(image)
+                        }
+                    }
+                }
+                self.tableView.reloadData()
+                
+            }
+        }
     }
 
   
